07a77f59b8877d89025e464cc266ead227754099
Roman Schader <me@rschader.com>
2021-12-10 14:51:06 +0100

    Add second of advent part 2

--- 02/advent-02.clj
+++ 02/advent-02.clj
@@ -8,20 +8,45 @@
   (-> (slurp file)
       (str/split-lines)))
 
-(defn dive [input]
+(defn part-1 [input]
   (let [positions (reduce (fn [acc curr]
                             (let [[command units] curr
-                                  depth (get acc :depth)
-                                  horizontal  (get acc :horizontal)]
+                                  depth (:depth acc)
+                                  horizontal  (:horizontal acc)]
                               (cond
                                 (= "forward" command) (assoc acc :horizontal (+ units horizontal))
                                 (= "down" command) (assoc acc :depth (+ units depth))
                                 :else (assoc acc :depth (- depth units)))))
                           {:horizontal 0 :depth 0}
                           input)]
-    (* (get positions :horizontal)
-       (get positions :depth))))
+    (* (:horizontal positions)
+       (:depth positions))))
 
-(let [input (read-file "resources/input_advent_02.txt")
-      measurements (map #(parse-pair %) input)
-      result (dive measurements)] result)
+(let [input (read-file "resources/input_advent_02.txt")]
+  (->
+   (map #(parse-pair %) input)
+   (part-1)))
+
+"================== part 2"
+
+(defn part-2 [input]
+  (let [positions (reduce (fn [acc curr]
+                            (let [[command units] curr
+                                  depth (:depth acc)
+                                  horizontal  (:horizontal acc)
+                                  aim  (:aim acc)]
+                              (cond
+                                (= "forward" command) (-> acc
+                                                          (assoc :horizontal (+ units horizontal))
+                                                          (assoc :depth (+ depth (* aim units))))
+                                (= "down" command) (assoc acc :aim (+ aim units))
+                                :else (assoc acc :aim (- aim units)))))
+                          {:horizontal 0 :depth 0 :aim 0}
+                          input)]
+    (* (:horizontal positions)
+       (:depth positions))))
+
+(let [input (read-file "resources/input_advent_02.txt")]
+  (->
+   (map #(parse-pair %) input)
+   (part-2)))
